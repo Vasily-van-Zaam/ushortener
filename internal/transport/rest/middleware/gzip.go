e072403c02fc15407cc51bbd28f0c288b7823f86
van-zaam <vasiliy.vanzaam@gmail.com>
2022-12-23 23:36:36 +0300

    added reader gzip data

--- internal/transport/rest/middleware/gzip.go
+++ internal/transport/rest/middleware/gzip.go
@@ -5,6 +5,7 @@ import (
 	"compress/gzip"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
@@ -21,9 +22,30 @@ func (w gzipResponseWriter) Write(b []byte) (int, error) {
 	return w.Writer.Write(b)
 }
 
+// func (w gzipResponseWriter) Read(b []byte) (int, error) {
+// 	// w.Writer будет отвечать за gzip-сжатие, поэтому пишем в него
+// 	return w.Read(b)
+// }
+
 func GzipHandle(next http.Handler, conf *core.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		bodyBytes, _ := io.ReadAll(r.Body)
+		var bodyBytes []byte
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			rg, errs := gzip.NewReader(r.Body)
+			if errs != nil {
+				log.Println("ERROR GZIP", errs)
+				w.WriteHeader(http.StatusBadRequest)
+				_, err := w.Write([]byte("error request data gzip "))
+				if err != nil {
+					log.Println("EERROR WRITE ", err)
+				}
+				next.ServeHTTP(w, r)
+				return
+			}
+			bodyBytes, _ = io.ReadAll(rg)
+		} else {
+			bodyBytes, _ = ioutil.ReadAll(r.Body)
+		}
 		r.Body.Close()
 		r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 
@@ -35,9 +57,10 @@ func GzipHandle(next http.Handler, conf *core.Config) http.Handler {
 
 		w.Header().Set("Content-Encoding", "gzip")
 
-		gz, _ := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		gz := gzip.NewWriter(w)
 		defer gz.Close()
 		gzr := gzipResponseWriter{Writer: gz, ResponseWriter: w}
+
 		conf.LogRequest(w, r, string(bodyBytes))
 		next.ServeHTTP(gzr, r)
 	})
